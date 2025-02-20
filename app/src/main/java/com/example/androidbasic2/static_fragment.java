4b7e72d926ab344455395f57d42f35dea0708b09
Ishaant Kumar Singh <ishaant97@gmail.com>
2025-02-20 13:56:06 +0530

    ToolBar and ScrollView Done

--- app/src/main/java/com/example/androidbasic2/static_fragment.java
+++ app/src/main/java/com/example/androidbasic2/static_fragment.java
@@ -2,12 +2,14 @@ package com.example.androidbasic2;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
 public class static_fragment extends Fragment {
-    public view onCreateView(LayoutInflater, ViewGroup container, Bundle savedInstanceState){
+    @Override
+    public View onCreateView(LayoutInflater, ViewGroup container, Bundle savedInstanceState){
         return inflater.inflate(R.layout.static_fragment, container, false);
     }
 }
