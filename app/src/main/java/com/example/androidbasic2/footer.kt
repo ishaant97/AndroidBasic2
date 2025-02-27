f7665524c1c107996bb4437ae23d8fac3205aaa3
Ishaant Kumar Singh <ishaant97@gmail.com>
2025-02-27 13:58:34 +0530

    Footer Done

--- app/src/main/java/com/example/androidbasic2/footer.kt
+++ app/src/main/java/com/example/androidbasic2/footer.kt
@@ -1,16 +1,25 @@
 package com.example.androidbasic2
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class footer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(R.layout.activity_footer)
 
+        val listView = findViewById<ListView>(R.id.listView)
+        val arr = arrayOf("Ishaant", "Shruti", "Abhishek", "Shashwat", "Priya", "Yousuf", "Aryan", "Archit")
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, arr)
+
+        val listFooter = layoutInflater.inflate(R.layout.footer_list, listView, false) as ViewGroup
+        listView.addFooterView(listFooter)
+        listView.adapter  = arrayAdapter
     }
 }
