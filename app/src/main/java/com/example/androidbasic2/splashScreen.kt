9b7133f3ccfb0bbb6f599cd6ae0f205baeabdb21
Ishaant Kumar Singh <ishaant97@gmail.com>
2025-03-07 23:38:51 +0530

    Splash Screen Done

--- app/src/main/java/com/example/androidbasic2/splashScreen.kt
+++ app/src/main/java/com/example/androidbasic2/splashScreen.kt
@@ -1,5 +1,6 @@
 package com.example.androidbasic2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,8 +10,13 @@ import androidx.core.view.WindowInsetsCompat
 class splashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
 
+        android.os.Handler().postDelayed({
+            val i = Intent(this@splashScreen, MainActivity::class.java)
+            startActivity(i)
+            finish()
+        },4000)
     }
 }
